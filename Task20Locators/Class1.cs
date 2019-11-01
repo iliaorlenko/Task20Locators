355945a7a46ff44130a40c8db649031bdf9c8cf6
Ilya Orlenko <ilia.orlenko@gmail.com>
2019-11-01 15:13:27 +0300

    init commit

---
+++ Task20Locators/Class1.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace Task20
+{
+    public class Locators
+    {
+        public By EnterLoginForm = By.ClassName("enter");
+        public By Login = By.CssSelector("input[type='text']");
+        public By Password = By.Name("password");
+        public By byId = By.Id("id");
+        public By byLinkText = By.LinkText("id");
+        public By byPartialTextLink = By.PartialLinkText("partialLink");
+    }
+}
---
+++ Task20Locators/TutByTests.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace Task20Locators
+{
+    [TestFixture]
+    public class TutByTests
+    {
+        [Test]
+        public void LoginWithValidCredentials()
+        {
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://tut.by";
+
+            Locators locators = new Locators();
+
+            driver.FindElement(locators.EnterLoginForm).Click();
+            driver.FindElement(locators.Login).SendKeys("test");
+            driver.FindElement(locators.Password).SendKeys("test");
+
+            System.Threading.Thread.Sleep(10000);
+
+        }
+    }
+}
